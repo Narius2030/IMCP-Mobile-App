a796b9514b94b6788d4a255323b14a664140bd47
HoangPhu0705 <phuhoang07051003@gmail.com>
2024-10-13 18:10:30 +0700

    feat(config): add neccessary dependencies, add camera screen

---
+++ client/lib/utils/colors.dart
@@ -0,0 +1,43 @@
+import 'dart:ui';
+
+class AppColors {
+  static const Color primary = Color(0xFF3F51B5);
+  static const Color primaryDark = Color(0xFF303F9F);
+  static const Color primaryLight = Color(0xFFC5CAE9);
+  static const Color accent = Color(0xFF00BCD4);
+  static const Color primaryText = Color(0xFF212121);
+  static const Color secondaryText = Color(0xFF757575);
+  static const Color divider = Color(0xFFBDBDBD);
+  static const Color white = Color(0xFFFFFFFF);
+  static const Color black = Color(0xFF000000);
+  static const Color red = Color(0xFFE57373);
+  static const Color green = Color(0xFF81C784);
+  static const Color blue = Color(0xFF64B5F6);
+  static const Color yellow = Color(0xFFFFEB3B);
+  static const Color orange = Color(0xFFFFAB91);
+  static const Color purple = Color(0xFF9575CD);
+  static const Color pink = Color(0xFFF06292);
+  static const Color teal = Color(0xFF4DB6AC);
+  static const Color brown = Color(0xFFA1887F);
+  static const Color grey = Color(0xFF90A4AE);
+  static const Color deepPurple = Color(0xFF512DA8);
+  static const Color indigo = Color(0xFF303F9F);
+  static const Color lightBlue = Color(0xFF4FC3F7);
+  static const Color cyan = Color(0xFF4DD0E1);
+  static const Color lightGreen = Color(0xFFAED581);
+  static const Color lime = Color(0xFFDCE775);
+  static const Color amber = Color(0xFFFFD54F);
+  static const Color deepOrange = Color(0xFFFF8A65);
+  static const Color blueGrey = Color(0xFF90A4AE);
+  static const Color grey50 = Color(0xFFFAFAFA);
+  static const Color grey100 = Color(0xFFF5F5F5);
+  static const Color grey200 = Color(0xFFEEEEEE);
+  static const Color grey300 = Color(0xFFE0E0E0);
+  static const Color grey400 = Color(0xFFBDBDBD);
+  static const Color grey500 = Color(0xFF9E9E9E);
+  static const Color grey600 = Color(0xFF757575);
+  static const Color grey700 = Color(0xFF616161);
+  static const Color grey800 = Color(0xFF424242);
+  static const Color grey900 = Color(0xFF212121);
+  static const Color blueGrey50 = Color(0xFFECEFF1);
+}
